e6b6144d264a8f910e577676705892bcfa6a7b9d
RubenVerhoeven <ruben.verhoeven2@hva.nl>
2025-09-24 09:44:57 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,28 @@
-var eersteStreepjesButton = document.querySelector("section.streepjes li:nth-of-type(1) button");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu-scherm");
+const menuLinks = document.querySelectorAll(".menu-inhoud nav a");
 
-eersteStreepjesButton.addEventListener("click", function() {
-  let expanded = eersteStreepjesButton.getAttribute("aria-expanded");
-
-  if (expanded === "false") {
-    eersteStreepjesButton.setAttribute("aria-expanded", "true");
-  } else {
-    eersteStreepjesButton.setAttribute("aria-expanded", "false");
-  }
+// Hamburger klik: toggle menu
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("aan");
+    const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
+    hamburger.setAttribute("aria-expanded", !expanded);
 });
 
+// Klik op menu link: menu sluiten
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("aan");
+        hamburger.setAttribute("aria-expanded", "false");
+    });
+});
+
+// Carousel click: center de afbeelding
+document.querySelectorAll('.carousel-track img').forEach(img => {
+    img.addEventListener('click', () => {
+        img.scrollIntoView({
+            behavior: 'smooth',
+            inline: 'center'
+        });
+    });
+});
